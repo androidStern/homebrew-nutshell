56e61cd0862dc85934ec8ac40b3cd59ef73b9688
Andrew Stern <andrew.martin.stern@gmail.com>
2026-05-25 13:29:31 -0500

    Add nutshell 0.1.0

---
+++ Formula/nutshell.rb
@@ -0,0 +1,26 @@
+class Nutshell < Formula
+  desc "Local personal trace ingestion runtime"
+  homepage "https://github.com/androidStern/nutshell"
+  url "https://github.com/androidStern/nutshell/releases/download/v0.1.0/nutshell-0.1.0-darwin-arm64.tar.gz"
+  version "0.1.0"
+  sha256 "6c39c84d7378cf169657ea09497bc0da69ec9b169794e7e1bd00fe4335412ce3"
+  license "MIT"
+
+  def install
+    bin.install "bin/nutshell"
+    prefix.install "Nutshell.app" if File.directory?("Nutshell.app")
+  end
+
+  def caveats
+    <<~EOS
+      Run `nutshell setup` after install. Protected-data sync is owned by Nutshell.app, not a Homebrew service.
+    EOS
+  end
+
+  test do
+    ENV["NUTSHELL_CONFIG"] = testpath/"nutconfig.jsonc"
+    ENV["NUTSHELL_ROOT"] = testpath/"Nutshell"
+    system bin/"nutshell", "--version"
+    assert_match "nutshell setup", shell_output("#{bin}/nutshell help")
+  end
+end
