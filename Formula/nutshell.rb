8afe93303f2421a2e5ea3dfb61e04ad914ae8e3d
Andrew Stern <andrew.martin.stern@gmail.com>
2026-05-30 23:57:37 -0500

    Update nutshell to 0.1.12

--- Formula/nutshell.rb
+++ Formula/nutshell.rb
@@ -1,9 +1,9 @@
 class Nutshell < Formula
   desc "Local personal trace ingestion runtime"
   homepage "https://github.com/androidStern/nutshell"
-  url "https://github.com/androidStern/nutshell/releases/download/v0.1.11/nutshell-0.1.11-darwin-arm64.tar.gz"
-  version "0.1.11"
-  sha256 "a27a6e25005852099d85b7d61eaeb35595ddeadac21c8364256888a9c23e8d1f"
+  url "https://github.com/androidStern/nutshell/releases/download/v0.1.12/nutshell-0.1.12-darwin-arm64.tar.gz"
+  version "0.1.12"
+  sha256 "f7a9c29fa3c82d3029349330a6f23025539312c9b16e6b78590b0c28db02c5cd"
   license "MIT"
 
   def install
